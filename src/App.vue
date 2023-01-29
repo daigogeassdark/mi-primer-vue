46cb8ed99997c8a9de847d7bd080eb635f8af519
daigogeassdark <yanezdiegom@gmail.com>
2023-01-28 19:47:32 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
  const name = 'Hola Proyecto Vue 3 v-on ejemplos '
 
-const click1 = () => {
-  console.log('click')
+const click1 = (message) => {
+  console.log(message)
 }
-const click2 = () => {
-  console.log('click2')
+const click2 = (message) => {
+  console.log(message)
 }
 </script>
 
 <template>
  <h1>Hola {{ name }}!</h1>
  <!--Click 1 avreviado-->
-  <button @click="click1" class="btn btn-info">Click1</button>
+  <button @click="click1('mensaje 1')" class="btn btn-info">Click1</button>
   <!--Click 2 completo-->
-  <button v-on:click="click2" class="btn btn-sucess">Click2</button>
+  <button v-on:click="click2('mensaje 2')" class="btn btn-sucess">Click2</button>
 </template>
 <style>
 button{
