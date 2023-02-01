dc0eba3353387d2556fb774fe827e444b7ba71c0
daigogeassdark <yanezdiegom@gmail.com>
2023-02-01 00:20:30 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
 import { ref, computed } from "vue";
 
-const name = "Vue 3";
+const name = "Vue 3 Desafio final del modulo";
 
 const counter = ref(0);
+
+const ArrayFavorites = ref([]);
+
+const addFavorite = () => {
+  ArrayFavorites.value.push(counter.value);
+};
+
+const disabledAddBtn = computed(() => {
+  const searchNum = ArrayFavorites.value.find((favorite) => favorite === counter.value);
+  console.log(searchNum)
+
+  //forma larga de hacer lo mismo
+  if (searchNum === 0) return true
+  return searchNum ? true : false;
+  //Forma mas corta de hacer lo mismo
+  // searchNum || searchNum === 0
+});
 
 const increment = () => {
     counter.value++;
@@ -27,13 +44,29 @@ const classCounter = computed(() => {
 </script>
 
 <template>
-    <h1>Hola {{ name }}!</h1>
-    <h2 :class="classCounter">
+  <div class="container text-container mt-2">
+    <h1 class="text-center">Hola {{ name }}!</h1>
+    <h2 :class="classCounter" class="text-center">
         {{ counter }}
     </h2>
-    <button @click="increment">Incremet</button>
-    <button @click="decrement">Decrement</button>
-    <button @click="reset">Reset</button>
+    <div class="btn-group text-center mx-auto">
+      <button @click="increment" class="btn btn-success" >Incremet</button>
+    <button @click="decrement" class="btn btn-warning">Decrement</button>
+    <button @click="reset" class="btn btn-danger">Reset</button>
+    <button @click="addFavorite" :disabled="disabledAddBtn" class="btn btn-info">Add to Favorite</button>
+    </div>
+    
+    <br>
+    <ul class="list-group mt-3">
+      <template v-for="favorite in ArrayFavorites" :key="favorite">
+        <li class="list-group-item">
+            {{ favorite }}
+        </li>
+      </template >
+        
+    </ul>
+  </div>
+   
 </template>
 
 <style>
