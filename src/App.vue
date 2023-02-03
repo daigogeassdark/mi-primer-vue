c646ab1dd8c7ea4444012982825bf333054b59c2
Diego Morejon <121526338+daigogeassdark@users.noreply.github.com>
2023-02-03 01:11:09 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,28 @@
-<script setup>
- const name = 'Hola Proyecto Vue 3 v-for y v-if + template'
- const arrayFrutas = [
-    {
-        name: "Manzana",
-        price: "$1.00",
-        description: "Una manzana",
-        stock: 0,
-    },
-    {
-        name: "Pera",
-        price: "$2.00",
-        description: "Una pera",
-        stock: 10,
-    },
-    {
-        name: "Naranja",
-        price: "$3.00",
-        description: "Una naranja",
-        stock: 20,
-    },
-];
-</script>
+<script >
+import { ref } from 'vue'
 
-<template>
- <h1>Hola {{ name }}!</h1>
-  <ul>
-      <template v-for="item in arrayFrutas" :key="item.name">
-        <li  v-if="item.stock > 0 ">
-          {{ item.name }} - {{ item.price }} - {{ item.description }} - {{ item.stock }}
-        </li>
-      </template>    
-  </ul>
-</template>
-<style>
-h1 {
-  color: red;
+export default {
+  // `setup` is a special hook dedicated for composition API.
+  setup() {
+    const count = ref(0)
+
+    function increment() {
+      count.value++
+    }
+
+    // expose the state to the template
+    return {
+      count,
+      increment
+    }
+  }
 }
-</style>
+
+</script>
+ <template>
+
+<h1>Wellcome to Fourth Module of Vue 3 Course; Reactividad sin atributo Setup</h1>
+
+<button @click="increment">{{ count }}</button>
+
+ </template>
